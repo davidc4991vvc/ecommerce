da9d64ba4d03a712648b8996f429426bcb13d03b
robinchoii <robinchoii@gmail.com>
2016-04-11 15:45:59 -0700

    partials and views

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+
+
+var User = require('./models/user');
+
+var app = express();
+
+mongoose.connect('mongodb://root:abc123@ds061415.mlab.com:61415/e-commerce', function(err) {
+  if (err) {
+    console.log(err);
+
+  } else {
+    console.log("Connected to the database")
+  }
+});
+
+// Middleware
+app.use(express.static(__dirname + '/public'))
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+
+
+app.post('/create-user', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err) {
+    if (err) return next(err);
+
+    res.json('Successfully created a new user')
+  })
+})
+
+
+app.get('/', function(req, res) {
+  res.render('main/home')
+});
+
+app.get('/about', function(req, res) {
+  res.render('main/about')
+})
+
+app.listen(3000, function(err) {
+  if (err) throw err;
+  console.log("Server is running on port 3000");
+});
