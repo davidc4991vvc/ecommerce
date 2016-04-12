cf09bfaef75b3ec95d1bba2b0c6d0cc3a3c213b0
robinchoii <robinchoii@gmail.com>
2016-04-11 20:23:08 -0700

    signup finished with flash

---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+  database: 'mongodb://root:abc123@ds061415.mlab.com:61415/e-commerce',
+  port: 3000,
+  secretKey: "Robin1337",
+
+}
--- server.js
+++ server.js
@@ -4,13 +4,18 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 
+
+var secret = require('./config/secret');
 var User = require('./models/user');
 
 var app = express();
 
-mongoose.connect('mongodb://root:abc123@ds061415.mlab.com:61415/e-commerce', function(err) {
+mongoose.connect(secret.database, function(err) {
   if (err) {
     console.log(err);
 
@@ -24,35 +29,25 @@ app.use(express.static(__dirname + '/public'))
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secret.secretKey
+}));
+app.use(flash());
+
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
+var mainRoutes = require('./routes/main');
+var userRoutes = require('./routes/user');
+app.use(mainRoutes);
+app.use(userRoutes);
 
 
-app.post('/create-user', function(req, res, next) {
-  var user = new User();
 
-  user.profile.name = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-
-  user.save(function(err) {
-    if (err) return next(err);
-
-    res.json('Successfully created a new user')
-  })
-})
-
-
-app.get('/', function(req, res) {
-  res.render('main/home')
-});
-
-app.get('/about', function(req, res) {
-  res.render('main/about')
-})
-
-app.listen(3000, function(err) {
+app.listen(secret.port, function(err) {
   if (err) throw err;
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port " + secret.port);
 });
