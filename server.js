e18da3164dc4b84c30bdca96f822715ad8c2dcc0
robinchoii <robinchoii@gmail.com>
2016-06-06 18:31:10 -0700

    prodcut page can add and minus quantity

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ var passport = require('passport');
 var secret = require('./config/secret');
 var User = require('./models/user');
 var Category = require('./models/category');
+
+var cartLength = require('./middlewares/middlewares');
+
 var app = express();
 
 mongoose.connect(secret.database, function(err) {
@@ -47,6 +50,8 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+app.use(cartLength);
 app.use(function(req, res, next) {
   Category.find({}, function(err, categories) {
     if(err) return next(eer);
