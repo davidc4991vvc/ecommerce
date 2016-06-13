a6628db45c6554317598dd234da3ca4a2fa3d77b
robinchoii <robinchoii@gmail.com>
2016-06-13 16:14:29 -0700

    add cart page, stripe api, checkout

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,13 +1,14 @@
 var Cart = require('../models/cart');
 
 
-module.exports = function( req, res, next) {
-  if(req.user) {
-    var total = 0
-    Cart.findOne({ owner: req.user._id}, function(err, cart) {
+module.exports = function(req, res, next) {
+
+  if (req.user) {
+    var total = 0;
+    Cart.findOne({ owner: req.user._id }, function(err, cart) {
       if (cart) {
         for (var i = 0; i < cart.items.length; i++) {
-           total += cart.items[i].quantity;
+          total += cart.items[i].quantity;
         }
         res.locals.cart = total;
       } else {
